463254e2f96eede76f3dbafc92947b9b5d442cb9
limyluy <luisroserodesarrollador@gmail.com>
2019-02-20 18:00:35 -0500

    final con algolia para testeo

--- app/src/main/java/com/example/agolia001/MainActivity.java
+++ app/src/main/java/com/example/agolia001/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Canvas;
 import android.os.Build;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.constraint.ConstraintLayout;
@@ -18,7 +19,11 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import com.algolia.instantsearch.core.events.QueryTextChangeEvent;
+import com.algolia.instantsearch.core.events.ResultEvent;
 import com.algolia.instantsearch.core.helpers.Searcher;
 import com.algolia.instantsearch.ui.helpers.InstantSearch;
 import com.algolia.instantsearch.ui.views.Hits;
@@ -44,86 +49,130 @@ import java.util.List;
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
-    Searcher searcher;
 
-
-    private static ArrayList<String> lista;
-
-    Hits hits;
-    SearchBox box;
-    boolean visible;
-
+    //variables estatica para Algolia
     final static String ALGOLIA_APP_ID = "LXY84IU2VY";
     final static String ALGOLIA_SEARCH_API_KEY = "7edbbd1e0f88f3b1176f3440ad018d3f";
     final static String ALGOLIA_INDEX_NAME = "productos";
 
+    //views
+    Hits hits;
+    SearchBox box;
+    ProgressBar progressBar;
+    ConstraintLayout layaut;
+
+    //variable para Algolia
+    Searcher searcher;
+
+    //Variables para funcionamiento
+     InstantSearch helper;
+    private int progressStatus = 0;
+    private Handler handler = new Handler();
+    SearchView.OnQueryTextListener xd;
 
 
-
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final Context context = this;
 
+        // find para views ancitivty main
+        hits = findViewById(R.id.hits);
+        box = findViewById(R.id.searchBox);
+        progressBar = findViewById(R.id.progressBar);
+        layaut = findViewById(R.id.contrain);
 
-        final Context context= this;
-         hits = findViewById(R.id.hits);
-         box = findViewById(R.id.searchBox);
-
+        // para hacer funcionar el buscador en algolia
         searcher = Searcher.create(ALGOLIA_APP_ID, ALGOLIA_SEARCH_API_KEY, ALGOLIA_INDEX_NAME);
-        final InstantSearch[] helper = new InstantSearch[1];
+        helper = new InstantSearch(this, searcher);
+        helper.search();
 
+        //para dar funcionamiento al progresbar y mostar el recycler(hits)
+        xd = new SearchView.OnQueryTextListener() {
 
-        box.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String s) {
                 hits.setVisibility(View.VISIBLE);
-                visible = true;
-                Log.e("onclink","si");
-                helper[0] = new InstantSearch((Activity) context, searcher);
-                helper[0].search();
+                progresbarr();
                 return false;
             }
-
             @Override
             public boolean onQueryTextChange(String s) {
-
                 return false;
             }
-        });
+        };
+
+
+
+        box.setOnQueryTextListener(xd);
 
 
 
 
-        ConstraintLayout layat = findViewById(R.id.contrain);
-
-        layat.setOnClickListener(new View.OnClickListener() {
+        //para cerrar recycler cn cualquier click afuera
+        layaut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.e("se cierra","si");
-                visible =false;
+                if (hits.getVisibility() == View.VISIBLE) {
+                    hits.setVisibility(View.INVISIBLE);
+                    progressBar.setVisibility(View.INVISIBLE);
+                    box.setOnQueryTextListener(xd);
+
+                }
+
             }
         });
 
 
-
-
-
     }
+    //para dar funcion al progresbar durante 3 segundos
+    void progresbarr() {
+        new Thread(new Runnable() {
+            public void run() {
+                while (progressStatus < 50) {
+                    progressStatus++;
+                    // Update the progress bar and display the
+                    //current value in the text view
+                    handler.post(new Runnable() {
+                        public void run() {
+                            progressBar.setVisibility(View.VISIBLE);
+
+                        }
+                    });
+                    try {
+                        // Sleep for 200 milliseconds.
+                        Thread.sleep(200);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                progressBar.setVisibility(View.INVISIBLE);
+            }
+        }).start();
+    }
+
+    //para cerrar recycler cuado de da tecla atras
     @Override
     public void onBackPressed() {
-        if (visible) {
-            Log.e("se cierra","si");
-        }else{
+        if (hits.getVisibility() == View.VISIBLE) {
+            hits.setVisibility(View.INVISIBLE);
+            progressBar.setVisibility(View.INVISIBLE);
 
-        super.onBackPressed();}
+            box.setOnQueryTextListener(xd);
+        } else {
+            super.onBackPressed();
+        }
+
     }
 
+    //para que el search se estruya
     @Override
     protected void onDestroy() {
         searcher.destroy();
         super.onDestroy();
     }
+
+
 }
+
